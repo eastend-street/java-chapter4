ba66dbf01e752e52137c680f8fdd3ed074d27dee
eastend-street <ss_1322irf@icloud.com>
2019-06-13 14:01:52 -0700

    made max program

---
+++ src/Max.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Max {
+	public static int max(int num1, int num2) {
+		int result = 0;
+
+		if (num1 > num2) {
+			result = num1;
+		} else {
+			result = num2;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter number 1: ");
+		int number1 = input.nextInt();
+
+		System.out.println("Enter number 2: ");
+		int number2 = input.nextInt();
+		System.out.println("Max number is " + max(number1, number2));
+	}
+}
